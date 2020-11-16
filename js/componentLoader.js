05c6b716d949eea8cc48892c47db3c54574ffd54
Haomin <haomin.yu@ucalgary.ca>
2020-11-15 21:59:36 -0700

    use relative path

--- js/componentLoader.js
+++ js/componentLoader.js
@@ -1,4 +1,4 @@
 $('document').ready(function() {
-    $('header').load('/components/header.html');
-    $('footer').load('/components/footer.html');
+    $('header').load('./components/header.html');
+    $('footer').load('./components/footer.html');
 });
--- js/router.js
+++ js/router.js
@@ -2,6 +2,6 @@ $('document').ready(function() {
     var route = window.location.href.split('route=')[1];
     console.log(route);
     if(!route) {
-        $('main').load('/pages/home.html');
+        $('main').load('./pages/home.html');
     }
 });
